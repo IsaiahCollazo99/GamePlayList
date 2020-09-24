755ca1945b009e0ce2b66662d242a1f6034ca683
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-09-24 02:05:29 -0400

    create schema

---
+++ backend/db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS gameplaylist_db;
+CREATE DATABASE gameplaylist_db;
+
+\c gameplaylist_db;
+
+DROP TABLE IF EXISTS playlist_games;
+DROP TABLE IF EXISTS playlists;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id VARCHAR UNIQUE PRIMARY KEY,
+    username VARCHAR,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    birthday VARCHAR,
+    email VARCHAR,
+    profile_picture VARCHAR,
+    gender VARCHAR
+);
+
+CREATE TABLE playlists (
+    id SERIAL PRIMARY KEY,
+    playlist_name VARCHAR,
+    playlist_owner VARCHAR REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE playlist_games (
+    id SERIAL PRIMARY KEY,
+    playlist_id INTEGER REFERENCES playlists(id) ON DELETE CASCADE,
+    game_id INTEGER
+);
