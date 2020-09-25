529210f02692dbeb9fffb786e8f5913d42a65245
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-09-25 15:47:30 -0400

    game card styling

--- frontend/src/features/gameCard/GameCard.js
+++ frontend/src/features/gameCard/GameCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../../css/gameCard/gameCard.css';
 
 const GameCard = ({ game }) => {
     return (
         <article className="gameCard">
             <p>{game.name}</p>
-            <img src={game.background_image} style={{width: '25%'}} alt={`${game.name} cover`} />
+            <img src={game.background_image} alt={`${game.name} cover`} />
         </article>
     )
 }
