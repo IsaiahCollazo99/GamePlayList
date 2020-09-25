cf2587483e05abc2f1fc8ba5d240a0d58ab30f53
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-09-25 03:31:57 -0400

    add createList and createDefaultList postRequests

--- frontend/src/util/apiCalls/postRequests.js
+++ frontend/src/util/apiCalls/postRequests.js
@@ -14,3 +14,23 @@ export const signUpUser = async ( userData ) => {
         throw error;
     }
 }
+
+export const createList = async ( list_name, list_owner ) => {
+    try {
+        const res = await axios.post(API + "/api/lists", {list_name, list_owner});
+        console.log(res);
+    } catch ( error ) {
+        throw error;
+    }
+}
+
+export const createDefaultLists = async ( userId ) => {
+    try {
+        const defaultLists = ["Owned", "Playing", "Shelved", "Wish List"];
+        defaultLists.forEach(async ( listName ) => {
+            await createList(listName, userId);
+        })
+    } catch ( error ) {
+        throw error;
+    }
+}
