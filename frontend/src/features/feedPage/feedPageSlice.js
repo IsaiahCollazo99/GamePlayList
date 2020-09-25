81fc22ba4a4cb101f67fe66a043e9e4ed968a507
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-09-25 00:02:13 -0400

    create feedPageSlice

---
+++ frontend/src/features/feedPage/feedPageSlice.js
@@ -0,0 +1,11 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+export const feedPageSlice = createSlice({
+    name: "feedPage",
+    initialState: {games: []},
+    reducers: {
+    
+    }
+})
+
+export default feedPageSlice.reducer;
