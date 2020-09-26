b45ccae8be41062b1b0be319e787e755ed5741fc
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-09-25 23:25:06 -0400

    add getUserByEmail request

--- backend/routes/users/users.js
+++ backend/routes/users/users.js
@@ -1,11 +1,13 @@
 const users = require("express").Router();
 const { 
-    signUp 
+    signUp,
+    getUserByEmail
 } = require("../../queries/users/users");
 const userLists = require("./userLists/userLists");
 
 users.use("/:id/lists", userLists);
 
 users.post("/", signUp);
+users.post("/email", getUserByEmail);
 
 module.exports = users;
