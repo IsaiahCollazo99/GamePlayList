6e50d676b4379be6f7c4fdab0225b01deb85e028
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-09-26 15:59:16 -0400

    add signIn styling and error handling

--- frontend/src/features/signIn/SignIn.js
+++ frontend/src/features/signIn/SignIn.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import { Button, TextField } from '@material-ui/core';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { isEmailExisting } from '../../util/apiCalls/postRequests';
 import { firebaseLogIn } from '../../util/firebaseFunctions';
 import { set_value } from './signInSlice';
+import '../../css/signIn/signIn.css';
 
 const SignIn = () => {
     const signIn = useSelector(state => state.signIn);
     const dispatch = useDispatch();
     const history = useHistory();
+    const [ errors, setErrors ] = useState({});
 
     const handleChange = ( e ) => {
         const stateToChange = e.target.name;
@@ -15,22 +19,85 @@ const SignIn = () => {
         dispatch(set_value({ stateToChange, data }));
     }
 
+    const isEmailExistingCall = async () => {
+        try {
+            const data = await isEmailExisting(signIn.email);
+            return data.user;
+        } catch ( error ) {
+            console.log(error);
+        }
+    }
+
     const handleSubmit = async ( e ) => {
         try { 
             e.preventDefault();
-            await firebaseLogIn(signIn.email, signIn.password);
-            history.push("/games");
+            if(!await isEmailExistingCall()) {
+                setErrors({...errors, email: "There is no record of this email."});
+            } else {
+                setErrors({});
+                await firebaseLogIn(signIn.email, signIn.password);
+                history.push("/games");
+            }
         } catch ( error ) {
+            if(error.code === "auth/wrong-password") {
+                setErrors({...errors, email: "This email/password combination is incorrect."});
+            } else if(error.code === "auth/user-not-found") {
+                setErrors({...errors, email: "There is no record of this email."});
+            }
             console.log(error);
         }
     }
     
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="email" name="email" onChange={handleChange} value={signIn.email} />
-            <input type="password" name="password" onChange={handleChange} value={signIn.password} />
-            <button type="submit">Log In</button>
-        </form>
+        <main className="signInContainer">
+            <h1>SIGN IN</h1>
+            <form onSubmit={handleSubmit}>
+                <TextField 
+                    type="email"
+                    label="Email"
+                    name="email"
+                    value={signIn.email}
+                    onChange={handleChange}
+                    error={errors.email ? true : false}
+                    helperText={errors.email ? errors.email : null}
+                    InputProps={{
+                        style: {
+                            color: 'white'
+                        }
+                    }}
+                    FormHelperTextProps={{
+                        style: {
+                            color: '#f44336'
+                        }
+                    }}
+                    autoFocus
+                    required
+                />
+
+                <TextField
+                    type="password"
+                    label="Password"
+                    name="password"
+                    value={signIn.password} 
+                    onChange={handleChange}
+                    InputProps={{
+                        style: {
+                            color: 'white'
+                        }
+                    }}
+                    required
+                />
+
+                <Button 
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                >
+                    Log In
+                </Button>
+            </form>
+        </main>
+        
     )
 }
 
