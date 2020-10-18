67d55ec420cbefff0ab05d47aa2fd24fc46b1b08
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-10-18 15:38:07 -0400

    side bar css

--- frontend/src/features/sideBar/SideBar.js
+++ frontend/src/features/sideBar/SideBar.js
@@ -1,12 +1,26 @@
+import { Typography } from '@material-ui/core';
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import '../../css/sideBar/sideBar.css';
 
 const SideBar = () => {
     const lists = useSelector(state => state.lists);
     console.log(lists);
+
+    const listsDisplay = lists.map(list => {
+        return (
+            <NavLink to={`/list/${list.id}`} key={list.id}>{list.list_name}</NavLink>
+        )
+    })
+
     return (
         <aside className="sideBar">
-
+            <h1>GPL</h1>
+            <section className="allUserLists">
+                <h2>Lists</h2>
+                {listsDisplay}
+            </section>
         </aside>
     )
 }
