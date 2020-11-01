d5df052c01f7f9fd14b39ce9ba27d593d559c659
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-10-31 20:20:45 -0400

    remove old backend, rename backendDjango to backend

--- backend/db/db.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-
-const pgp = require("pg-promise")({});
-const db = pgp(process.env.DATABASE_URL);
-
-module.exports = db;
--- backend/routes/users/userLists/userLists.js
+++
@@ -1,6 +0,0 @@
-const userLists = require("express").Router({mergeParams: true});
-const { getUserLists } = require("../../../queries/users/userLists/userLists");
-
-userLists.get("/", getUserLists);
-
-module.exports = userLists;
