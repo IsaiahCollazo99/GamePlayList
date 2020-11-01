1701f9887716762a10df629d90fe271608a6bbf9
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-11-01 16:25:05 -0500

    change all backendDjango calls to djangoSettings

--- djangoSettings/asgi.py
+++ djangoSettings/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for backendDjango project.
+ASGI config for djangoSettings project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backendDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoSettings.settings')
 
 application = get_asgi_application()
