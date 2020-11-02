a74c1dc73527c44a3628274faf002dc2c191e798
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-11-02 13:18:28 -0500

    change AuthRoute redirect to / from /games

--- frontend/src/util/routesUtil.js
+++ frontend/src/util/routesUtil.js
@@ -8,7 +8,7 @@ export const AuthRoute = ( { children, ...rest } ) => {
         <Route 
             {...rest}
             render={( { location } ) => {
-                return !currentUser ? children : <Redirect to="/games" />
+                return !currentUser ? children : <Redirect to="/" />
             }}
         />
 
